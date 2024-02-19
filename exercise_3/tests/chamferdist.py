ab7fb094de290242a987fb75a32ea0e61ac362eb
ataatasoy <h.ataatasoy@gmail.com>
2024-02-19 18:45:55 +0100

    Final tests

--- exercise_3/tests/chamferdist.py
+++ exercise_3/tests/chamferdist.py
@@ -8,3 +8,88 @@
 # The paper used 30K points for distance measurement we also can use the same number OR since paper calculated SDFs with 16K points and we used 1/4 of it, we can use 10K points
 
 # Calculate chamfer distance https://www.fwilliams.info/point-cloud-utils/sections/shape_metrics/
+
+import argparse
+
+import torch
+import numpy as np
+
+# pip install trimesh[all]
+import trimesh
+
+import point_cloud_utils as pcu
+
+# https://github.com/otaheri/chamfer_distance
+from chamfer_distance import ChamferDistance
+
+def as_mesh(scene_or_mesh):
+    if isinstance(scene_or_mesh, trimesh.Scene):
+        assert len(scene_or_mesh.geometry) > 0
+        mesh = trimesh.util.concatenate(
+            tuple(trimesh.Trimesh(vertices=g.vertices, faces=g.faces)
+                for g in scene_or_mesh.geometry.values()))
+    else:
+        assert isinstance(scene_or_mesh, trimesh.Trimesh)
+        mesh = scene_or_mesh
+    return mesh
+
+def sample_mesh(m, n):
+    vpos, _ = trimesh.sample.sample_surface(m, n)
+    return torch.tensor(vpos, dtype=torch.float32, device="cuda")
+
+''''
+def get_chamfer_distance(mesh, ref_vertices, ref_faces, n):
+    chamfer_dist = ChamferDistance()
+    mesh = as_mesh(trimesh.load(mesh))
+    ref = trimesh.Trimesh(vertices=ref_vertices, faces=ref_faces)
+
+    # Sample mesh surfaces
+    vpos_mesh = sample_mesh(mesh, n)
+    vpos_ref = sample_mesh(ref, n)
+
+    # Make sure l=1.0 maps to 1/10th of the AABB. https://arxiv.org/pdf/1612.00603.pdf
+    scale = 10.0 / np.amax(np.amax(ref.vertices, axis=0) - np.amin(ref.vertices, axis=0))
+    ref.vertices = ref.vertices * scale
+    mesh.vertices = mesh.vertices * scale
+
+    print(f"vpos_mesh: {vpos_mesh.shape}")
+    dist1, dist2, idx1, idx2 = chamfer_dist(vpos_mesh[None, ...], vpos_ref[None, ...])
+    loss = (torch.mean(dist1)) + (torch.mean(dist2))
+
+    torch.cuda.synchronize()
+    print(f"Loss: {loss}")
+
+
+    #dist1, dist2 = chamfer_dist(vpos_mesh[None, ...], vpos_ref[None, ...])
+    #loss = (torch.mean(dist1) + torch.mean(dist2)).item()
+    
+    #print("[%7d tris]: %1.5f" % (mesh.faces.shape[0], loss))
+
+'''
+
+def get_chamfer_distance(mesh_path_1, mesh_path_2):
+    
+    # p1 is an (n, 3)-shaped numpy array containing one point per row
+    p1 = pcu.load_mesh_v(mesh_path_1)
+
+    # p2 is an (m, 3)-shaped numpy array containing one point per row
+    p2 = pcu.load_mesh_v(mesh_path_2)
+
+    # Compute the chamfer distance between p1 and p2
+    cd = pcu.chamfer_distance(p1, p2)
+
+    print(f"Chamfer Distance: {cd}")
+    return cd
+
+def get_sinkhorn_distance(mesh_path_1, mesh_path_2):
+
+    # p1 is an (n, 3)-shaped numpy array containing one point per row
+    p1 = pcu.load_mesh_v(mesh_path_1)
+
+    # p2 is an (m, 3)-shaped numpy array containing one point per row
+    p2 = pcu.load_mesh_v(mesh_path_2)
+
+    # Compute the chamfer distance between p1 and p2
+    emd, pi = pcu.earth_movers_distance(p1, p2)
+
+    return emd
---
+++ test.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+import numpy as np
+import matplotlib as plt
+from exercise_3.inference.infer_deepsdf import InferenceHandlerDeepSDF
+import k3d
+import trimesh
+import torch
+import skimage
+import argparse
+
+from exercise_3.data.shape_implicit import ShapeImplicit
+from exercise_3.util.visualization import visualize_mesh, visualize_pointcloud
+from exercise_3.model.deepsdf import DeepSDFDecoder
+from exercise_3.util.model import summarize_model
+from exercise_3.training import train_deepsdf
+from exercise_3.tests.chamferdist import get_chamfer_distance, get_sinkhorn_distance
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--category", help="Specify a category from ['chair', 'picture_frame_painting', 'sofa', 'rug']", required=True)
+    parser.add_argument("--experiment_name", help="Specify a category from ['chair', 'picture_frame_painting', 'sofa', 'rug']", required=True)
+    parser.add_argument("--experiment_type", help="Specify a category from ['vanilla', 'pe', 'vn']", required=True)
+
+    args = parser.parse_args()
+
+    experiment_type = args.experiment_type
+    experiment_name = args.experiment_name
+    shape_class = args.category
+
+    device = torch.device('cuda:0')  # change this to cpu if you're not using a gpu
+
+    inference_handler = InferenceHandlerDeepSDF(256, f"/home/atasoy/project/dsdf/exercise_3/runs/{experiment_name}", device, num_encoding_functions=2, experiment_type=experiment_type)
+
+    #Get the val.txt file for the shape class
+    val_file = f'/home/atasoy/project/dsdf/exercise_3/data/splits/{shape_class}/val.txt'
+    shape_ids = Path(val_file).read_text().splitlines()
+    num_val_shapes = len(shape_ids)
+    chamfer_dist = 0
+    #sinkhorn_dist = 0
+
+    for shape_id in shape_ids:
+
+        try:
+            print(f"Processing shape {shape_id}")
+            mesh_path = f'/mnt/hdd/atasoy_dataset/ml3d/{shape_class}/{shape_id}/mesh.obj'
+            mesh = trimesh.load(mesh_path)
+            # Calculate the centroid of the mesh
+            centroid = mesh.centroid
+            # Translate the mesh so that the centroid is at the origin
+            mesh = mesh.apply_translation(-centroid)
+
+            # Step 2: Scale the mesh to fit within a unit sphere
+            # Calculate the furthest distance from the origin to any vertex
+            max_distance = np.max(np.linalg.norm(mesh.vertices, axis=1))
+            # Calculate the scaling factor (the mesh will be scaled so that the furthest point is 1 unit away)
+            scaling_factor = 1.0 / max_distance
+            # Scale the mesh
+            mesh = mesh.apply_scale(scaling_factor)
+
+            file_path_orig = '/home/atasoy/project/dsdf/try_mesh_orig.obj'
+            with open(file_path_orig, 'w') as file:
+                mesh.export(file_obj=file, file_type='obj')
+
+            
+            points, sdf = ShapeImplicit.get_all_sdf_samples(shape_id, shape_class)
+            vertices, faces = inference_handler.reconstruct(points, sdf, 800)
+            ref = trimesh.Trimesh(vertices=vertices, faces=faces)
+            # Calculate the centroid of the mesh
+            centroid = ref.centroid
+            # Translate the mesh so that the centroid is at the origin
+            ref = ref.apply_translation(-centroid)
+
+            # Step 2: Scale the mesh to fit within a unit sphere
+            # Calculate the furthest distance from the origin to any vertex
+            
+            max_distance = np.max(np.linalg.norm(ref.vertices, axis=1))
+            
+            # Calculate the scaling factor (the mesh will be scaled so that the furthest point is 1 unit away)
+            scaling_factor = 1.0 / max_distance
+            # Scale the mesh
+            ref = ref.apply_scale(scaling_factor)
+            file_path = '/home/atasoy/project/dsdf/try_mesh.obj'
+            with open(file_path, 'w') as file:
+                ref.export(file_obj=file, file_type='obj')
+            chamfer_dist += get_chamfer_distance(file_path_orig, file_path)
+            print(f"Chamfer distance for shape {shape_id}, {chamfer_dist}")
+            #sinkhorn_dist += get_sinkhorn_distance(mesh_path, file_path)
+            #print(f"Chamfer distance for shape {shape_id}, {chamfer_dist}")
+        except ValueError:  #raised if `y` is empty.
+                pass
+
+    print(f"Mean Chamfer Distance: {chamfer_dist/num_val_shapes}, for experiment {experiment_name}")
+    #print(f"Mean Sinkhorn Distance: {sinkhorn_dist/num_val_shapes}, for experiment {experiment_name}")
+
+
+
+if __name__ == "__main__":
+    main()
