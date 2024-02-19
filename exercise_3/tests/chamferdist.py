ac377305b3c73ea6e8b6a5bea7112a34b7de9714
ataatasoy <h.ataatasoy@gmail.com>
2024-02-19 09:32:37 +0100

    Template for chamfer and earthmover distances

---
+++ exercise_3/tests/chamferdist.py
@@ -0,0 +1,10 @@
+#TODO: Implement Chamfer Distance Metric w/ G.T Mesh
+
+# Read the GT meshes
+
+# Read the generated meshes from the validation set
+
+# Generate random points samples https://www.fwilliams.info/point-cloud-utils/sections/mesh_sampling/#generating-random-samples-on-a-mesh
+# The paper used 30K points for distance measurement we also can use the same number OR since paper calculated SDFs with 16K points and we used 1/4 of it, we can use 10K points
+
+# Calculate chamfer distance https://www.fwilliams.info/point-cloud-utils/sections/shape_metrics/
---
+++ exercise_3/tests/earthmoverdistance.py
@@ -0,0 +1,11 @@
+#TODO: Implement Earthmover Distance Metric w/ G.T Mesh
+
+# Read the GT meshes
+
+# Read the generated meshes from the validation set
+
+# Generate random points samples https://www.fwilliams.info/point-cloud-utils/sections/mesh_sampling/#generating-random-samples-on-a-mesh
+# The paper used 500 points to calculate this metric
+
+# Calculate chamfer distance https://www.fwilliams.info/point-cloud-utils/sections/shape_metrics/
+~                                                                                                  
